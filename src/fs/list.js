f4c46f98582083938d49c8f14664f9dd0776cd31
webmisfit <76257594+webmisfit@users.noreply.github.com>
2022-12-19 20:53:10 +0600

    fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import path from 'path';
+import fs from 'fs';
+import { dirName, fileName, exist } from '../functionality/index.js';
+
+// implement function that prints all array of filenames from files folder into console
+//(if files folder doesn't exists Error with message FS operation failed must be thrown)
+
 const list = async () => {
-    // Write your code here 
+  const __filename = fileName(import.meta.url);
+  const __dirname = dirName(__filename);
+  const dirPath = path.join(__dirname, 'files');
+  try {
+    if (!(await exist(dirPath))) throw new Exception();
+
+    fs.readdir(dirPath, (err, files) => {
+      console.log(files);
+    });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import fs from 'fs';
+import { dirName, fileName, exist } from '../functionality/index.js';
+
+// implement function that prints content of the fileToRead.txt into console
+//(if there's no file fileToRead.txt Error with message FS operation failed must be thrown)
+
 const read = async () => {
-    // Write your code here 
+  const __filename = fileName(import.meta.url);
+  const __dirname = dirName(__filename);
+  const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+  try {
+    if (!(await exist(filePath))) throw new Exception();
+    console.log(await (await fs.promises.readFile(filePath)).toString());
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await read();
