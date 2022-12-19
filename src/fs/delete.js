7aefdf9ec930b22a85bb20d92161a95ab95faad8
webmisfit <76257594+webmisfit@users.noreply.github.com>
2022-12-19 20:39:30 +0600

    fs:rename

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { dirName, fileName, exist } from '../functionality/index.js';
+
+// implement function that deletes file fileToRemove.txt
+// (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)
+
 const remove = async () => {
-    // Write your code here 
+  const __filename = fileName(import.meta.url);
+  const __dirname = dirName(__filename);
+  const filesPath = path.join(__dirname, 'files', 'fileToRemove.txt');
+  try {
+    if(!await exist(filesPath)) throw new Exception();
+    await fs.promises.unlink(filesPath)
+  } catch {
+    throw new Error('FS operation failed')
+  }
 };
 
 await remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,21 @@
+import path from 'path';
+import fs from 'fs';
+import { dirName, fileName, exist } from '../functionality/index.js';
+
+// implement function that renames file wrongFilename.txt to properFilename with extension .md
+// (if there's no file wrongFilename.txt or properFilename.md already exists Error with message FS operation failed must be thrown)
+
 const rename = async () => {
-    // Write your code here 
+  const __filename = fileName(import.meta.url);
+  const __dirname = dirName(__filename);
+  const filePath = path.join(__dirname, 'files', 'wrongFilename.txt');
+  const renamedPath = path.join(__dirname, 'files', 'properFilename.md');
+  try {
+    if (!await exist(filePath) || await exist(renamedPath)) throw new Exception();
+    await fs.promises.rename(filePath, renamedPath)
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
